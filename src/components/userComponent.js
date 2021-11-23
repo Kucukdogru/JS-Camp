ddce38ad13624683d3251711d69f93fbc471ec1d
Kucukdogru <m42erve@hotmail.com>
2021-11-23 23:28:28 +0300

    22 Kasım Ders Sonu

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,4 +1,5 @@
 import { BaseLogger, ElasticLogger } from "../crossCuttingConcerns/logging/logger.js";
+import Customer from "../models/customer.js";
 import User from "../models/user.js";
 import UserService from "../services/userService.js";
 
@@ -22,3 +23,13 @@ let customer = { id: 1, firstName: "Merve" }
 //prototype
 customer.lastName = "KUCUKDOGRU"
 console.log(customer.lastName);
+
+console.log("---------------------");
+userService.load();
+let customerToAdd = new Customer(1, "Deniz", "BBB", "Konya", 25 )
+customerToAdd.type = "customer"
+userService.add(customerToAdd)
+console.log(userService.customers);
+console.log(userService.employees);
+console.log(userService.errors);
+console.log(userService.getCustomersSorted());
--- src/models/user.js
+++ src/models/user.js
@@ -4,10 +4,11 @@ export default class User {
     //     console.log("User oluştu");
     // }
 
-    constructor(id, firstName, lastName, city) {
+    constructor(id, firstName, lastName, city, age) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.city = city
+        this.age = age
     }
 }
